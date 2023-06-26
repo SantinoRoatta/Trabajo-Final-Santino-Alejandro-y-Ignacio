81a2f30e27a8988e51b97fd3096116a27a6e4d66
Nicolás Olmos <80790398+NicolasOlmos222@users.noreply.github.com>
2023-06-26 19:44:14 -0300

    Carga de archivos

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+function comprarViaje() {
+    window.location.href = "comprar viaje new york.html";
+  }
+  
+function opcion2() {
+    window.location.href = "formulariocompranewyork.html"; 
+}
+function opcion1(){
+  window.location.href = "comprarviajenewyorkPayPal.html"; 
+}
